8ea931e08cc3184323e03f71102cb52a2e411764
Akmaral <axma.mail@gmail.com>
2022-10-10 12:27:57 +0600

    Задача 22

---
+++ Ex004/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 22. Напишите программу которая принимает на вход число (N)
+// и выдает таблицу квадратов чисел от 1 до N.
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= n; i++)
+    Console.Write(i * i + " ");
+    // можно вывод записать подругому: 
+    // Console.Write($"{i * i} ");
