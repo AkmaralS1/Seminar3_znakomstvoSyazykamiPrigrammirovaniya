87a62bf26ca062310cf34c045f5ec0820d946911
Akmaral <axma.mail@gmail.com>
2022-10-10 11:55:40 +0600

    Задача 18

---
+++ Ex002/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 18. Напишите программу которая по заданному номеру четверти
+// показывает диапазон возможных координат в этой четверти (х и у).
+
+Console.Clear();
+Console.Write("Введите номер четверти: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n == 1)
+    Console.WriteLine("x > 0 and y > 0");
+else if (n == 2)
+    Console.WriteLine("x < 0 and y > 0");
+else if (n == 3)
+    Console.WriteLine("x < 0 and y < 0");
+else 
+    Console.WriteLine("x > 0 and y < 0");
